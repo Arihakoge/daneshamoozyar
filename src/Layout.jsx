23d8192d46b02b8dd9e534c64ba5285869bc613e
aradh1609 <aradh1609@gmail.com>
2025-11-29 15:14:40 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -185,6 +185,7 @@ export default function Layout({ children, currentPageName }) {
         { title: "ویرایش پروفایل", url: createPageUrl("EditProfile"), icon: Edit },
         { title: "تابلوی امتیازات", url: createPageUrl("Scoreboard"), icon: Trophy },
         { title: "یارا - دستیار هوشمند", url: createPageUrl("YaraChat"), icon: MessageCircle },
+        { title: "راهنما", url: createPageUrl("Help"), icon: HelpCircle },
       ];
     } else if (role === "teacher") {
       return [
